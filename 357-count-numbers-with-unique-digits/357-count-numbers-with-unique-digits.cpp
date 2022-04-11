03302e547d427a3ae99632d286cef739352b640c
Li Zuming <lokiscripter@outlook.com>
2022-04-11 22:58:06 +0800

    Modify Reason: 357 | count-numbers-with-unique-digits l https://leetcode-cn.com/problems/count-numbers-with-unique-digits

---
+++ 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.cpp
@@ -0,0 +1,16 @@
+//
+// Created by loki on 2022/4/11.
+//
+
+#include "../common.h"
+
+int countNumbersWithUniqueDigits(int n) {
+    if (n == 0) return 1;
+    vector<int> dp(n+1);
+    dp[0] = 1;
+    dp[1] = 10;
+    for (int i = 2; i <= n; ++i) {
+        dp[i] = dp[i-1] + (dp[i-1]-dp[i-2])*(10-(i-1));
+    }
+    return dp[n];
+}
